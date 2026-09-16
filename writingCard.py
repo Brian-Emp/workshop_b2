ccccce781054855a684ffcd1cce59aba060cb09f
RG <231309571+RgEpsi@users.noreply.github.com>
2026-09-16 21:12:18 +0200

    Maj writingCard.py
    Ajout : entrer le nom du nouveau propriétaire si le badge n'est pas assigné.

--- writingCard.py
+++ writingCard.py
@@ -3,10 +3,24 @@ from mfrc522 import SimpleMFRC522
 
 reader = SimpleMFRC522()
 
-try: 
-    data_to_write = "A changer lors de la prod"
-    print("Place your card to write data...")
+try:
+    print("Place your card to read/write data...")
+    
+    
+    uid = reader.read_id()
+    print(f"Badge détecté, ID : {uid}")
+    
+
+    if uid == 481729585450:
+        data_to_write = "M. Larbin"
+    elif uid == 145033995888: 
+        data_to_write = "M. Pickle Rick"
+    else:
+        data_to_write = input("Entrez le nom du propriétaire pour le badge : ")
+        
+    print("Laissez le badge sur le lecteur pour l'écriture...")
     reader.write(data_to_write)
-    print("Data written to card successfully!")
+    print(f"Succès ! '{data_to_write}' a été écrit sur la carte.")
+
 finally:
     GPIO.cleanup()
